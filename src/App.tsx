bc2eb76e6441084f015f7c3f5c8f51b8b9454798
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2021-11-05 19:05:49 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "styles/global.scss";
+import "./i18n";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
