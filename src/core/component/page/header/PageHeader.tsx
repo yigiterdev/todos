c424fd816821f877db451fd7964df906484844e3
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2021-12-08 16:53:25 +0300

    feat(page-header): Use theme switch component

--- src/core/component/page/header/PageHeader.tsx
+++ src/core/component/page/header/PageHeader.tsx
@@ -1,32 +1,13 @@
 import "./_page-header.scss";
 
-import {Switch} from "@hipo/react-ui-toolkit";
-import {MdModeNight} from "react-icons/md";
-import {BsSunFill} from "react-icons/bs";
-
-import {useAppContext} from "../../../context/AppContext";
+import ThemeSwitch from "../../theme-switch/ThemeSwitch";
 
 function PageHeader() {
-  const {
-    appState: {theme},
-    dispatchAppStateReducerAction
-  } = useAppContext();
-
   return (
     <header className={"page-header"}>
-      <div className={"page-header__theme-select"}>
-        <Switch isToggledOn={theme === "dark-theme"} onToggle={handleSwitch} />
-
-        {theme === "dark-theme" ? <BsSunFill /> : <MdModeNight />}
-      </div>
+      <ThemeSwitch />
     </header>
   );
-
-  function handleSwitch() {
-    dispatchAppStateReducerAction({
-      type: "CHANGE_THEME"
-    });
-  }
 }
 
 export default PageHeader;
