0901452d429a21809db2b105e87932664da2bc77
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2021-12-26 19:40:29 +0300

    feat(todocard): try to implement grid layout

--- src/home/dashboard/HomePageDashboard.tsx
+++ src/home/dashboard/HomePageDashboard.tsx
@@ -1,16 +1,30 @@
 import "./_home-page-dashboard.scss";
 
-import AddTodoCard from "./components/add-todo/AddTodoCard";
+import {List, ListItem} from "@hipo/react-ui-toolkit";
+
+// import AddTodoCard from "./components/add-todo/AddTodoCard";
+import {useAppContext} from "../../core/context/AppContext";
+import Todocard from "./components/todocard/Todocard";
 
 interface HomePageDashboardProps {
   customClassName?: string;
 }
 
 function HomePageDashboard({customClassName}: HomePageDashboardProps) {
+  const {
+    appState: {todoCards}
+  } = useAppContext();
+
   return (
     <div className={customClassName}>
       <div className={"home-page-dashboard__content"}>
-        <AddTodoCard />
+        <List items={todoCards} customClassName={"home-page-dashboard__content__items"}>
+          {(item) => (
+            <ListItem>
+              <Todocard todocardData={item} />
+            </ListItem>
+          )}
+        </List>
       </div>
     </div>
   );
--- src/home/dashboard/components/todocard/Todocard.tsx
+++ src/home/dashboard/components/todocard/Todocard.tsx
@@ -1,5 +1,18 @@
-function Todocard() {
-  return <div>{"CARD"}</div>;
+import "./_todocard.scss";
+
+import {TodoCard} from "../../../../core/context/types";
+
+interface TodocardProps {
+  todocardData: TodoCard;
+}
+
+function Todocard({todocardData}: TodocardProps) {
+  return (
+    <div className={"todocard"}>
+      <h1>{todocardData.title}</h1>
+      <p>{todocardData.userId}</p>
+    </div>
+  );
 }
 
 export default Todocard;
